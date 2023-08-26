023d62ca34d146914ffba256a01de6c4b9e63577
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-08-26 16:01:16 +0600

    added for deployment problem

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-app.use(express.json());
+// app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
